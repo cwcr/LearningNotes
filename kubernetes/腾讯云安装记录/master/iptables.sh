df072ea9c4d3f54e99e26abcda3856a3a6cf4d3c
王也 <ye.wang@hand-china.com>
2019-07-02 11:30:01 +0800

    增加openvpn安装方法

--- kubernetes/腾讯云安装记录/master/iptables.sh
+++ kubernetes/腾讯云安装记录/master/iptables.sh
@@ -1,3 +1,17 @@
+#!/bin/bash
+service iptables status 1>/dev/null 2>&1
+if [ $? -eq 0 ]; then
 iptables -A INPUT -p tcp --dport 6443 -j ACCEPT      # 开放6443端口
-iptables -A INPUT -p tcp --dport 2379:2380 -j ACCEPT
-iptables -A INPUT -p tcp --dport 10250:10255 -j ACCEPT
+iptables -A INPUT -p tcp --dport 2379:2380 -j ACCEPT # 开放2379-2380端口
+iptables -A INPUT -p tcp --dport 10250:10255 -j ACCEPT #开放10250-10255端口
+echo "open port by iptables"
+fi
+
+service firewalld status 1>/dev/null 2>&1
+if [ $? -eq 0 ]; then
+firewall-cmd --permanent --zone=public --add-port=6443/tcp --permanent
+firewall-cmd --permanent --zone=public --add-port=2379-2380/tcp --permanent
+firewall-cmd --permanent --zone=public --add-port=10250-10255/tcp --permanent
+firewall-cmd --reload
+echo "open port by firewalld"
+fi
